7190af3da18dc20f594922abf60d0eb5f060edef
Jonathan Holt <jonathan.a.holt@gmail.com>
2017-06-24 20:18:42 +0100

    Inital commit

---
+++ handler.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+/**
+ * [index default server function to allow all rources to be called]
+ * @param  {object}   event     [event calling the resource]
+ * @param  {object}   context   [context with event]
+ * @param  {Function} callback  [action to be called on return]
+ * @return {void}               [no return]
+ */
+const index = (event, context, callback) => {
+  const type = event.pathParameters.type;
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      type,
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
+
+export {
+  index
+};
